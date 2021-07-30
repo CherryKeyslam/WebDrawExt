06c8dc8a5772e2452eebcaf2bb2c873ff635683b
CherryKeyslam <85491783+CherryKeyslam@users.noreply.github.com>
2021-07-30 15:35:48 +0530

    Update test.js

--- test.js
+++ test.js
@@ -4,17 +4,32 @@ canvasEl.height = window.innerHeight - 10;
 
 var art_piece = canvasEl.getContext("2d");
 
+var clicking = false;
 var previousX = 0;
 var previousY = 0;
 
 art_piece.lineWidth = 20;
+art_piece.lineCap = "round";
 
 canvasEl.addEventListener("mousemove", function(e) {
-	art_piece.beginPath();
-	art_piece.moveTo(previousX,previousY);
-	art_piece.lineTo(e.clientX,e.clientY);
-	art_piece.stroke();
-	console.log("X: " + e.clientX.toString() + " Y: " + e.clientY.toString())
+	if(clicking == true)
+	{
+		art_piece.beginPath();
+		art_piece.moveTo(previousX,previousY);
+		art_piece.lineTo(e.clientX,e.clientY);
+		art_piece.stroke();
+		previousX = e.clientX;
+		previousY = e.clientY;
+	}
+})
+canvasEl.addEventListener("mousedown", function(e) {
 	previousX = e.clientX;
 	previousY = e.clientY;
+	clicking = true;
+})
+canvasEl.addEventListener("mouseup", function(e) {
+	clicking = false;
+})
+canvasEl.addEventListener("mouseleave", function(e) {
+	clicking = false;
 })
